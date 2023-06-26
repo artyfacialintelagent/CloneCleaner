e5d2cff3a8822c49784c8752650b702d21355ff0
artyfacialintelagent <artyfacialintelagent@gmail.com>
2023-06-26 17:09:13 +0200

    First beta release

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # CloneCleaner
- An extension for Automatic1111 to work around Stable Diffusion's "clone problem". It automatically modifies your prompts with random names, nationalities and hair to create more variations in generated people.
+
+An extension for Automatic1111 to work around Stable Diffusion's "clone problem". It automatically modifies your prompts with random names, nationalities, hair style and hair color to create more variations in generated people.
+
+# What it does
+
+Many generations of model finetuning and merging have greatly improved the image quality of Stable Diffusion 1.5 when generating humans - but at the cost of overtraining and loss of variability. This manifests as "clones", where batch generations using the same or similar prompts but different random seeds often have identical facial features.
+
+CloneCleaner adds randomized tokens to your prompt that varies the look of a person for every generated image seed. For example, one seed might get "Elsie from Australia, long waist-length updo ginger hair" and the next "Samreen from Bangladesh, medium shoulder-length frizzy coffee-colored hair". This makes every seed quite unique and effectively mitigates the "sameface" problem of many popular but heavily overtrained Stable Diffusion models. So it's basically wildcards, except the tiresome setup work has been done for you and you're ready to go - with lots of easy customization options to control the randomization.
+
+A key detail is that the token randomization seed is (by default) identical to the main image seed - this ensures that the same "person" will be generated again if you modify the prompt or reload the metadata.
+
+# Installation in Automatic1111
+
+Enter this url **manually** in auto1111's extension tab:
+
+https://github.com/artyfacialintelagent/CloneCleaner.git
+
+This first release of CloneCleaner is a public beta and **currently only works for female characters**. Options for male (and indeterminate gender if I can get it to work) coming soon-ish!
+
+# How it works
+
+Prompts are randomized using wildcards, except they're hardcoded in the extension with logic to match ethnicity with typical names and common hair colors for each country, in order to get consistent appearance and quality of the generated images. Main steps:
+
+1. Set the token randomization seed to the main image seed (or optionally a different random seed or a user-specified one).
+2. Select a random REGION among the following: Europe (mainland incl. Russia), Anglosphere (US, Can, Aus, NZ, UK), Latin America, MENA (Middle-East & North Africa), Sub-Saharan Africa, East Asia or South Asia.
+3. Select a random COUNTRY in that region - but note that CloneCleaner only has a sample of countries of each region, the database is not (yet) comprehensive.
+4. Select a random FIRST NAME for people in that country.
+5. Select a random MAIN HAIR COLOR (only black, brown, red, blonde, other), weighted for what is typical in that country. Then select a SPECIFIC HAIR COLOR with more "colorful" (sorry) language for more variability. Color tokens are carefully selected and limited using tricks like reduced attention and prompt editing to minimize "color bleeding" into the rest of your prompt.
+6. Select random HAIR STYLE and HAIR LENGTH.
+7. Assemble the prompt insert: "[FIRST NAME] from [COUNTRY], [HAIR LENGTH] [HAIR STYLE] [SPECIFIC HAIR COLOR] hair". Insert it at beginning or end of the prompt depending on user options
+8. Iterate for remaining images of the batch.
+
+Any of the above token components can be optionally excluded from the prompt randomization. There are also customization options to exclude specific regions, hair lengths and hair color.
+
+# Sample images
+
+The following images are produced using **consecutive seeds**, so they are **NOT CHERRY-PICKED** in any way. But to be fair, not all models work quite as well as these - some models are so overtrained that they just can't be saved. The sample PNGs hosted here on Github should contain full metadata, so you can download and inspect them in the PNGinfo tab, or send them to txt2img to reproduce them.
+
+Right-click and select "Open image in a new tab" to view at 100%, or right-click and select "Save image as" to download.
+
+### Absolute Reality v1
+
+These images were created using the [Absolute Reality model](https://civitai.com/models/81458/absolutereality), from a simple test prompt I made up.
+
+**Absolute Reality v1 (baseline model images)**
+![absolutereality_seeds](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/04fe1106-8fcd-4a60-866d-03cf2636fa94)
+![absolutereality1](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/698de3a0-2bb7-43b3-bd3e-7c1c75b38d61)
+
+**Absolute Reality v1 + CloneCleaner (default settings)**
+![absolutereality2](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/8efdffae-45f5-47e4-8544-8345435fe1ee)
+
+**Absolute Reality v1 + CloneCleaner (East Asia only)**
+![absolutereality3](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/7059ec65-1f25-4155-bab5-968f6291b994)
+
+**Absolute Reality v1 + CloneCleaner (Anglosphere + Europe only, short hair only)**
+![absolutereality4](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/149ee7fe-f745-4cf0-af48-46d6d54db59e)
+
+### Photon v1
+
+Using the prompt from the sample image hosted on the [Photon model page on Civitai](https://civitai.com/models/84728/photon), slightly modified to make it more SFW for Github.
+
+**Photon v1 (baseline model images)**
+![photon_seeds](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/4d66add9-fd6a-42b8-aea1-271e674af951)
+![photon1](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/bc586d3f-d4f1-4ffa-9859-40ae2deb6660)
+
+**Photon v1 + CloneCleaner (default settings)**
+![photon2](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/3f2cac67-79a4-4067-8b52-57cda7c6bfdf)
+
+**Photon v1 + CloneCleaner (Africa + South Asia only, no blonde or "other" hair color)**
+![photon3](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/3d8a6379-1bd8-4c59-8059-26241530d254)
+
+**Photon v1 + CloneCleaner (Europe only, reddish hair only)**
+![photon4](https://github.com/artyfacialintelagent/CloneCleaner-dev/assets/137619889/69a295f3-4347-4135-820c-ed62ee3cba0e)
+
+# Some tips
+
+I am very happy with how well this simple prompt modification scheme works. But the best part about CloneCleaner is how it made me completely re-evaluate my opinion of many models, mostly positively. So be sure to retest your models using CloneCleaner - they may yet surprise you!
+
+I recommend using **simple prompts** (< 50 tokens), **simple negatives** (< 30 tokens) and **limited attention weighting** (never > :1.2, except as noted below). An effective minimal negative prompt appears below. Just start with this as a basis and add whatever your image seems to need.
+
+**Negative prompt**: *ugly, asian, underage, 3d render, cartoon, doll, (bad low worst quality:1.3)*
+
+The token "asian" is included to counter the heavy bias towards Korean and Chinese women among most popular models. Asian characters should still appear and look perfectly fine even including this token. Usually the attention setting of the final quality prompt should stick to the range 1.1 - 1.4, but a small number of Asian-oriented models can benefit from high (~1.5) or extreme values (up to 2.0!). Note that this is the exception that proves the rule - in most models such extreme weights would heavily "overcook" your images and destroy both quality and variability.
+
+I rarely use attention weights above 1.0 in my prompts and **never** use attention weights above 1.2 for any other tokens in my prompt other than this general quality negative. In my experience this greatly benefits image consistency and reduces mutations, bad hands and other monstrosities without having to explicitly include these things in your negatives.
---
+++ scripts/clonecleaner.py
@@ -0,0 +1,223 @@
+import gradio as gr
+import os
+import random
+import sys
+import yaml
+
+from modules import scripts, script_callbacks, shared, paths
+from modules.processing import Processed
+from modules.ui_components import FormRow, FormColumn, FormGroup, ToolButton
+from modules.ui import random_symbol, reuse_symbol, gr_show
+from modules.generation_parameters_copypaste import parse_generation_parameters
+from pprint import pprint
+
+def read_yaml():
+    promptfile = os.path.join(scripts.basedir(), "prompt_tree.yml")
+    with open(promptfile, "r", encoding="utf8") as stream:
+        prompt_tree = yaml.safe_load(stream)
+        return prompt_tree
+
+def get_last_params(declone_seed, gallery_index):
+    filename = os.path.join(paths.data_path, "params.txt")
+    if os.path.exists(filename):
+        with open(filename, "r", encoding="utf8") as file:
+            prompt = file.read()
+
+    if gallery_index > 0:
+        gallery_index -= 1
+    params = parse_generation_parameters(prompt)
+    if params.get("CC_use_main_seed", "") == "True":
+        return [int(float(params.get("Seed", "-0.0"))) + gallery_index, gr_show(False)]
+    else:
+        return [int(float(params.get("CC_declone_seed", "-0.0"))) + gallery_index, gr_show(False)]
+
+def sorted_difference(a, b):
+    newlist = list(set(a).difference(b))
+    newlist.sort()
+    return newlist
+
+class CloneCleanerScript(scripts.Script):
+    prompt_tree = read_yaml()   # maybe make this an instance property later
+
+    def title(self):
+        return "CloneCleaner"
+
+    # show menu in either txt2img or img2img
+    def show(self, is_img2img):
+        return scripts.AlwaysVisible
+    
+    def ui(self, is_img2img):
+        with gr.Accordion("CloneCleaner (beta!)", open=True):
+            dummy_component = gr.Label(visible=False)
+            regions = self.prompt_tree["country"].keys()
+            hairlength = self.prompt_tree["hair"]["length"].keys()
+            haircolor = self.prompt_tree["hair"]["color"].keys()
+            with FormRow():
+                with FormColumn(min_width=160):
+                    is_enabled = gr.Checkbox(value=True, label="Enable CloneCleaner")
+                with FormColumn(elem_id="CloneCleaner_gender"):
+                    gender = gr.Radio(["female", "male", "generic"], value="female", label="Male & generic not yet implemented.", elem_classes="ghosted")
+                    gender.style(container=False, item_container=False)
+            with FormRow(elem_id="CloneCleaner_components"):
+                components = ["name", "country", "hair length", "hair style", "hair color"]
+                use_components = gr.CheckboxGroup(components, label="Use declone components", value=components)
+            with FormRow(elem_id="CloneCleaner_midsection"):
+                with FormGroup():
+                    insert_start = gr.Checkbox(value=True, label="Put declone tokens at beginning of prompt")
+                    declone_weight = gr.Slider(minimum=0.0, maximum=2.0, step=0.05, value=1.0, label="Weight of declone tokens", elem_id="CloneCleaner_slider")
+                with FormGroup():
+                    use_main_seed = gr.Checkbox(value=True, label="Use main image seed for decloning")
+                    with FormRow(variant="compact", elem_id="CloneCleaner_seed_row", elem_classes="ghosted"):
+                        declone_seed = gr.Number(label='Declone seed', value=-1, elem_id="CloneCleaner_seed")
+                        declone_seed.style(container=False)
+                        random_seed = ToolButton(random_symbol, elem_id="CloneCleaner_random_seed", label='Random seed')
+                        reuse_seed = ToolButton(reuse_symbol, elem_id="CloneCleaner_reuse_seed", label='Reuse seed')
+            with FormRow(elem_id="CloneCleaner_exclude_row") as exclude_row:
+                exclude_regions = gr.Dropdown(choices=regions, label="Exclude regions", multiselect=True)
+                exclude_hairlength = gr.Dropdown(choices=hairlength, label="Exclude hair lengths", multiselect=True)
+                exclude_haircolor = gr.Dropdown(choices=haircolor, label="Exclude hair colors", multiselect=True)
+        
+        jstoggle = "() => {document.getElementById('CloneCleaner_seed_row').classList.toggle('ghosted')}"
+        jsclickseed = "() => {setRandomSeed('CloneCleaner_seed')}"
+        jsgetgalleryindex = "(x, y) => [x, selected_gallery_index()]"
+        other_jstoggles =   "() => {" + \
+                    "const labels = document.getElementById('CloneCleaner_components').getElementsByTagName('label');" + \
+                    "const excludelabels = document.getElementById('CloneCleaner_exclude_row').getElementsByTagName('label');"  + \
+                    "excludelabels[1].classList.toggle('ghosted', !labels[2].firstChild.checked);" + \
+                    "excludelabels[2].classList.toggle('ghosted', !labels[4].firstChild.checked);" + \
+                    "}"
+        use_main_seed.change(fn=None, _js=jstoggle)
+        random_seed.click(fn=None, _js=jsclickseed, show_progress=False, inputs=[], outputs=[])
+        reuse_seed.click(fn=get_last_params, _js=jsgetgalleryindex, show_progress=False, inputs=[declone_seed, dummy_component], outputs=[declone_seed, dummy_component])
+        use_components.change(fn=None, _js=other_jstoggles)
+        
+        def list_from_params_key(key, params):
+            regionstring = params.get(key, "")
+            regions = regionstring.split(",") if regionstring else []
+            return gr.update(value = regions)
+
+        self.infotext_fields = [
+            (is_enabled, "CloneCleaner enabled"),
+            (gender, "CC_gender"),
+            (insert_start, "CC_insert_start"),
+            (declone_weight, "CC_declone_weight"),
+            (use_main_seed, "CC_use_main_seed"),
+            (declone_seed, "CC_declone_seed"),
+            (exclude_regions, lambda params:list_from_params_key("CC_exclude_regions", params)),
+            (exclude_hairlength, lambda params:list_from_params_key("CC_exclude_hairlength", params)),
+            (exclude_haircolor, lambda params:list_from_params_key("CC_exclude_haircolor", params))
+        ]
+        return [is_enabled, gender, insert_start, declone_weight, use_main_seed, declone_seed, use_components, exclude_regions, exclude_hairlength, exclude_haircolor]
+
+    def process(self, p, is_enabled, gender, insert_start, declone_weight, use_main_seed, declone_seed, use_components, exclude_regions, exclude_hairlength, exclude_haircolor):
+        if not is_enabled:
+            return
+
+        if use_main_seed:
+            declone_seed = p.all_seeds[0]
+        elif declone_seed == -1:
+            declone_seed = int(random.randrange(4294967294))
+        else:
+            declone_seed = int(declone_seed)
+
+        # original_prompt = p.all_prompts[0]
+        # settings = f"gender={gender}, beginning={insert_start}, declone_weight={declone_weight}, main_seed={use_main_seed}, " + \
+        #             f"declone_seed={declone_seed}, exclude_regions={exclude_regions}"
+        p.extra_generation_params["CloneCleaner enabled"] = True
+        p.extra_generation_params["CC_gender"] = gender
+        p.extra_generation_params["CC_insert_start"] = insert_start
+        p.extra_generation_params["CC_declone_weight"] = declone_weight
+        p.extra_generation_params["CC_use_main_seed"] = use_main_seed
+        p.extra_generation_params["CC_declone_seed"] = declone_seed
+        if exclude_regions:
+            p.extra_generation_params["CC_exclude_regions"] = ",".join(exclude_regions)
+        if exclude_hairlength:
+            p.extra_generation_params["CC_exclude_hairlength"] = ",".join(exclude_hairlength)
+        if exclude_haircolor:
+            p.extra_generation_params["CC_exclude_haircolor"] = ",".join(exclude_haircolor)
+
+        countrytree = self.prompt_tree["country"]
+        hairtree = self.prompt_tree["hair"]
+
+        regions = sorted_difference(countrytree.keys(), exclude_regions)
+        hairlengths = sorted_difference(hairtree["length"].keys(), exclude_hairlength)
+        haircolors = sorted_difference(hairtree["color"].keys(), exclude_haircolor)
+
+        use_name = "name" in use_components
+        use_country = "country" in use_components
+        use_length = "hair length" in use_components
+        use_style = "hair style" in use_components
+        use_color = "hair color" in use_components
+
+        for i, prompt in enumerate(p.all_prompts):      # for each image in batch
+            rng = random.Random()
+            seed = p.all_seeds[i] if use_main_seed else declone_seed + i
+            rng.seed(seed)
+
+            region = rng.choice(regions)
+            countries = list(countrytree[region].keys())
+            countryweights = [countrytree[region][cty]["weight"] for cty in countries]
+            country = rng.choices(countries, weights=countryweights)[0]
+
+            countrydata = countrytree[region][country]
+            hairdata = countrydata.get("hair", hairtree["defaultweight"][region])
+            maincolor = rng.choices(haircolors, weights=[hairdata[col] for col in haircolors])[0]
+            color = rng.choice(hairtree["color"][maincolor])
+            mainlength = rng.choice(hairlengths)
+            length = rng.choice(hairtree["length"][mainlength])
+            style = rng.choice(hairtree["style"][mainlength])
+            name = rng.choice(countrydata["names"])
+
+            inserted_prompt = ""
+
+            if use_name or use_country:
+                inserted_prompt += name if use_name else "person"
+                inserted_prompt += " from " + country if use_country else ""
+            
+            if use_length or use_style or use_color:
+                if inserted_prompt:
+                    inserted_prompt += ", "
+                if use_length:
+                    inserted_prompt += length + " "
+                if use_style:
+                    inserted_prompt += style + " "
+                if use_color:
+                    inserted_prompt += color + " "
+                inserted_prompt += "hair"
+
+            if inserted_prompt:
+                if declone_weight != 1:
+                    inserted_prompt = f"({inserted_prompt}:{declone_weight})"
+
+                if insert_start:
+                    p.all_prompts[i] = inserted_prompt + ", " + prompt
+                else:
+                    p.all_prompts[i] = prompt + ", " + inserted_prompt
+    
+    # def postprocess_batch(self, p, *args, **kwargs):
+    #     p.all_prompts[0] = p.prompt    # gets saved in file metadata AND in batch file metadata
+
+    # def process_batch(self, p, *args, **kwargs):
+    #     p.extra_generation_params["CC_TEST"] = "whatever"
+    #     p.all_prompts[0] = p.prompt + " SUFFIX"
+
+    def postprocess(self, p, processed, *args):
+        with open(os.path.join(paths.data_path, "params.txt"), "w", encoding="utf8") as file:
+            p.all_prompts[0] = p.prompt
+            processed = Processed(p, [], p.seed, "")
+            file.write(processed.infotext(p, 0))
+
+# read with shared.opts.prompt_database_path
+def on_ui_settings():
+    info = shared.OptionInfo("prompt_tree.yml", "CloneCleaner prompt database path", section=("clonecleaner", "CloneCleaner"))
+    shared.opts.add_option("prompt_database_path", info)
+    # shared.opts.add_option("option1", shared.OptionInfo(
+    #     False,
+    #     "option1 description",
+    #     gr.Checkbox,
+    #     {"interactive": True},
+    #     section=('template', "Template"))
+    # )
+
+
+script_callbacks.on_ui_settings(on_ui_settings)
